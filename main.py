74d6961ac6e2f19a7a933c18d0091688b3dbc24a
zhous <zhoushengfromphy@hotmail.com>
2022-08-04 22:51:37 +0200

    v1 time series SCRM total cost

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from statsmodels.tsa.deterministic import DeterministicProcess, CalendarFourier
 import xgboost as xgb
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 
+### CoSupply data preprocessing :
 # hr = pd.read_csv('CoSupplyChain/DataCoSupplyChainDataset.csv')
 # # hr.info()
 # # hrs = hr.select_dtypes('number')
@@ -33,15 +39,55 @@ from sklearn.metrics import accuracy_score
 #
 # hrs.to_csv('CoSupplyChain/PrepDataset.csv', sep=',', encoding='utf-8')
 
-hr = pd.read_csv('CoSupplyChain/PrepDataset.csv')
-hr.info()
-X = hr.drop(['order date (DateOrders)', 'shipping date (DateOrders)',
-             'Delivery Status', 'Late_delivery_risk'], axis=1)
-y = hr['Late_delivery_risk']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+# ### model training :
+# hr = pd.read_csv('CoSupplyChain/PrepDataset.csv')
+# hr.info()
+# X = hr.drop(['order date (DateOrders)', 'shipping date (DateOrders)',
+#              'Delivery Status', 'Late_delivery_risk'], axis=1)
+# y = hr['Late_delivery_risk']
 
-model = xgb.XGBClassifier(max_depth=4, n_estimators=100).fit(X_train, y_train)
-print("Accuracy == ", accuracy_score(y_test, model.predict(X_test)))
+### SCRM data preprocessing :
+hr = pd.read_csv('SCRM/SCRM_timeSeries_2018_test.csv', parse_dates=['Timestamp'], infer_datetime_format=True)
+# hr['Datetime_parsed'] = pd.to_datetime(hr['Timestamp'], format='%Y/%m/%d', infer_datetime_format=True)
+print(hr.dtypes)
+hr.Timestamp = hr.Timestamp.dt.to_period('T')
+print(hr.head(20))
+print('\n'+'-'*20)
+print(hr.describe())
+
+hrt = hr.copy()
+y = hrt.Total_Cost.dropna() #SCMstability_category
+dp = DeterministicProcess(index=y.index, constant=True, order=2, drop=True)
+X = dp.in_sample()
+# X = hrt.drop(columns=['Timestamp', 'SCMstability_category'], axis=1).fillna(0.0)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+
+## It will be easier for us later if we split the date index instead of the dataframe directly.
+idx_train, idx_test = train_test_split(y.index, test_size=0.1, shuffle=False)
+X_train, X_test = X.loc[idx_train, :], X.loc[idx_test, :]
+y_train, y_test = y.loc[idx_train], y.loc[idx_test]
+
+## Fit trend model
+model = LinearRegression(fit_intercept=False)
+model.fit(X_train, y_train)
+
+## Make predictions
+y_fit = pd.DataFrame(model.predict(X_train), index=y_train.index)
+y_pred = pd.DataFrame(model.predict(X_test), index=y_test.index)
+
+## Plot
+axs = y_train.plot(color='0.25', subplots=True, sharex=True)
+axs = y_test.plot(color='0.25', subplots=True, sharex=True, ax=axs)
+axs = y_fit.plot(color='C0', subplots=True, sharex=True, ax=axs)
+axs = y_pred.plot(color='C3', subplots=True, sharex=True, ax=axs)
+for ax in axs: ax.legend([])
+_ = plt.suptitle("Trends")
+plt.show()
+
+
+# model = DecisionTreeClassifier(max_depth=4).fit(X_train, y_train)
+# model = xgb.XGBClassifier(max_depth=4, n_estimators=100).fit(X_train, y_train)
+# print("Accuracy == ", accuracy_score(y_test, model.predict(X_test)))
 
 
 # Press the green button in the gutter to run the script.
