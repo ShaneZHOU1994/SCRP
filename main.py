e6a5baea45f52019241f869df428a65ad38a3e2e
zhous <zhoushengfromphy@hotmail.com>
2022-10-03 22:17:57 +0200

    v4.0 simu dataset by Chenhui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from funcs import *
+from IPython.display import display
 import seaborn as sns
 import matplotlib.pyplot as plt
 from statsmodels.tsa.deterministic import DeterministicProcess, CalendarFourier
@@ -10,6 +11,70 @@ from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 from sklearn.metrics import balanced_accuracy_score, log_loss, roc_auc_score
+
+
+def read_data_simu():
+    hr = pd.read_excel('Simu_w34.xlsx', header=[0,1,2,3], nrows=34, keep_default_na=False)
+    hr.replace('', float('NaN'), inplace=True)
+    hr.dropna(how='all', axis=1, inplace=True)
+
+    ## remove unnamed levels :
+    columns = pd.DataFrame(hr.columns.tolist())
+    for i in range(len(columns.columns)):
+        columns.loc[columns[i].str.startswith('Unnamed:'), i] = '_'
+    hr.columns = pd.MultiIndex.from_tuples(columns.to_records(index=False).tolist())
+    hr.columns = hr.columns.remove_unused_levels()
+    ## expand multiindex :
+    hr.columns = [' '.join(col).strip() for col in hr.columns.values if col != '_']
+    cols = hr.columns
+    return hr
+
+
+def calcul_risk_simu(hr):
+    ## Calculate Risk Indices :
+    ## Factory F1 and F2:
+    hr['CSR_F1'] = (hr['F1 Sale Price w1 BJ']*hr['F1 Sale w1 BJ']+hr['F1 Sale Price w2 BJ']*hr['F1 Sale w2 BJ']
+                    +hr['F1 Sale Price w3 BJ']*hr['F1 Sale w3 BJ']+hr['F1 Sale Price w1 PO']*hr['F1 Sale w1 PO']
+                    +hr['F1 Sale Price w2 PO'] * hr['F1 Sale w2 PO']+hr['F1 Sale Price w3 PO']*hr['F1 Sale w3 PO'])\
+                   /(hr['F1 Receive SA1 B']*hr['F1 Buy Price SA1 B']+hr['F1 Receive SA1 P']*hr['F1 Buy Price SA1 P']
+                     +hr['F1 Receive SA2 B']*hr['F1 Buy Price SA2 B']+hr['F1 Receive SA2 P']*hr['F1 Buy Price SA2 P']
+                     +hr['F1 Receive SB1 PE']*hr['F1 Buy Price SB1 PE']+hr['F1 Receive SB1 O']*hr['F1 Buy Price SB1 O']
+                     +hr['F1 Receive SB2 PE']*hr['F1 Buy Price SB2 PE']+hr['F1 Receive SB2 O']*hr['F1 Buy Price SB2 O']) - 1
+    hr['PPR_F1'] = (hr['F1 Receive SA1 B']/hr['F1 Demand SA1 B']+hr['F1 Receive SA1 P']/hr['F1 Demand SA1 P']
+                   +hr['F1 Receive SA2 B']/hr['F1 Demand SA2 B']+hr['F1 Receive SA2 P']/hr['F1 Demand SA2 P']
+                   +hr['F1 Receive SB1 PE']/hr['F1 Demand SB1 PE']+hr['F1 Receive SB1 O']/hr['F1 Demand SB1 O']
+                   +hr['F1 Receive SB2 PE']/hr['F1 Demand SB2 PE']+hr['F1 Receive SB2 O']/hr['F1 Demand SB2 O'])/8
+    hr['OR_F1'] = (hr['F1 Product _ BJ']/(hr['F1 Order w1 BJ']+hr['F1 Order w2 BJ']+hr['F1 Order w3 BJ'])
+                   +hr['F1 Product _ PO']/(hr['F1 Order w1 PO']+hr['F1 Order w2 PO']+hr['F1 Order w3 PO']))/2
+    hr['OCR_F1'] = (hr['F1 Order w1 BJ'].shift(1).fillna(0)+hr['F1 Order w2 BJ'].shift(1).fillna(0)
+                    +hr['F1 Order w3 BJ'].shift(1).fillna(0)+hr['F1 Order w1 PO'].shift(1).fillna(0)
+                    +hr['F1 Order w2 PO'].shift(1).fillna(0)+hr['F1 Order w3 PO'].shift(1).fillna(0)) / \
+                   (hr['F1 Order w1 BJ']+hr['F1 Order w2 BJ']+hr['F1 Order w3 BJ']\
+                   +hr['F1 Order w1 PO']+hr['F1 Order w2 PO']+hr['F1 Order w3 PO']) - 1
+    hr['SSR_F1'] = (hr['F1 Sale w1 BJ']+hr['F1 Sale w2 BJ']+hr['F1 Sale w3 BJ']+hr['F1 Sale w1 PO']
+                    +hr['F1 Sale w2 PO']+hr['F1 Sale w3 PO']) / (hr['F1 Demand Stock BJ']+hr['F1 Demand Stock PO'])
+    t_es = 3
+    hr['ITO_F1'] = (hr['F1 Sale Price w1 BJ']*hr['F1 Sale w1 BJ']+hr['F1 Sale Price w2 BJ']*hr['F1 Sale w2 BJ']
+                    +hr['F1 Sale Price w3 BJ']*hr['F1 Sale w3 BJ']+hr['F1 Sale Price w1 PO']*hr['F1 Sale w1 PO']
+                    +hr['F1 Sale Price w2 PO'] * hr['F1 Sale w2 PO']+hr['F1 Sale Price w3 PO']*hr['F1 Sale w3 PO'])
+    for i in range(1, t_es+1):
+        hr['ITO_F1'] += (hr['F1 Sale Price w1 BJ'].shift(i).fillna(0)*hr['F1 Sale w1 BJ'].shift(i).fillna(0)
+                    +hr['F1 Sale Price w2 BJ'].shift(i).fillna(0)*hr['F1 Sale w2 BJ'].shift(i).fillna(0)
+                    +hr['F1 Sale Price w3 BJ'].shift(i).fillna(0)*hr['F1 Sale w3 BJ'].shift(i).fillna(0)
+                    +hr['F1 Sale Price w1 PO'].shift(i).fillna(0)*hr['F1 Sale w1 PO'].shift(i).fillna(0)
+                    +hr['F1 Sale Price w2 PO'].shift(i).fillna(0)*hr['F1 Sale w2 PO'].shift(i).fillna(0)
+                    +hr['F1 Sale Price w3 PO'].shift(i).fillna(0)*hr['F1 Sale w3 PO'].shift(i).fillna(0))
+    hr['ITO_F1'] /= (hr['F1 Demand Stock BJ']*(hr['F1 Sale Price w1 BJ']+hr['F1 Sale Price w2 BJ']+hr['F1 Sale Price w3 BJ'])/3
+                     +hr['F1 Demand Stock PO']*(hr['F1 Sale Price w1 PO']+hr['F1 Sale Price w2 PO']+hr['F1 Sale Price w3 PO'])/3
+                     +hr['F1 Demand Stock BJ'].shift(t_es).fillna(0)*(hr['F1 Sale Price w1 BJ'].shift(t_es).fillna(0)
+                    +hr['F1 Sale Price w2 BJ'].shift(t_es).fillna(0)+hr['F1 Sale Price w3 BJ'].shift(t_es).fillna(0))/3
+                     +hr['F1 Demand Stock PO'].shift(t_es).fillna(0)*(hr['F1 Sale Price w1 PO'].shift(t_es).fillna(0)
+                    +hr['F1 Sale Price w2 PO'].shift(t_es).fillna(0)+hr['F1 Sale Price w3 PO'].shift(t_es).fillna(0))/3)/2
+
+    # display(hr)
+    X = hr.drop(['CSR_F1','PPR_F1','OR_F1','OCR_F1','SSR_F1','ITO_F1'], axis=1)
+    y = (hr['CSR_F1']>=(hr['CSR_F1'].mean())).astype(int)
+    return X, y
 
 
 def prep_data_CoSupply():
@@ -39,7 +104,6 @@ def prep_data_CoSupply():
 
 
 def read_data_CoSupply(nskip=10000, nrow=10000):
-    ## model training :
     hr = pd.read_csv('CoSupplyChain/PrepDataset.csv', skiprows=range(1, nskip), nrows=nrow)
     hr.pop(hr.columns[0])
     # hr.info()
@@ -60,14 +124,13 @@ def read_data_CoSupply(nskip=10000, nrow=10000):
             agg_func.update({feat: np.mean})
     hr = hr.groupby(['order date (DateOrders)']).agg(agg_func)
 
-    X = hr.drop(['Days for shipping (real)', 'Days for shipment (scheduled)',
-                 'Delivery Status', 'Late_delivery_risk'], axis=1)
+    X = hr.drop(['Days for shipping (real)', 'Days for shipment (scheduled)', 'Delivery Status', 'Late_delivery_risk'], axis=1)
     y = hr['Late_delivery_risk']
     return X, y
 
 
 def xgb_clf_cosupply(X, y, nlag=4, nlead=4):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=False)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
     ## Make lag features
     X_train = make_lags(X_train, lags=nlag).fillna(0.0)
     X_test = make_lags(X_test, lags=nlag).fillna(0.0)
@@ -76,7 +139,7 @@ def xgb_clf_cosupply(X, y, nlag=4, nlead=4):
     X_train, y_train = X_train.align(y_train, join='inner', axis=0)
     X_test, y_test = X_test.align(y_test, join='inner', axis=0)
     ## Learn model
-    model = XGBClassifier(objective='binary:logistic', max_depth=4, n_estimators=100)#'multi:softprob'
+    model = XGBClassifier(objective='binary:logistic', max_depth=4, n_estimators=30)#'multi:softprob'
     model.fit(X_train, y_train)
     ## Add the predicted residuals onto the predicted trends
     y_fit = model.predict(X_train)
@@ -224,12 +287,17 @@ def eval_clf_result(y_train, y_test, y_fit, y_pred, e_train, e_test, p_fit, p_pr
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # hr = read_data_simu()
+    # X, y = calcul_risk_simu(hr)
+    # y_train, y_test, y_fit, y_pred, e_train, e_test, p_fit, p_pred = xgb_clf_cosupply(X, y, nlag=3, nlead=1)
+    # eval_clf_result(y_train, y_test, y_fit, y_pred, e_train, e_test, p_fit, p_pred)
+    # pass
+
     ## CoSupply dataset:
     # prep_data_CoSupply()
     X, y = read_data_CoSupply(nskip=10000, nrow=2000)
     y_train, y_test, y_fit, y_pred, e_train, e_test, p_fit, p_pred = xgb_clf_cosupply(X, y, nlag=5, nlead=1)
     eval_clf_result(y_train, y_test, y_fit, y_pred, e_train, e_test, p_fit, p_pred)
-
 
     ## SCRM dataset:
     # hr, hrt, nstep = read_data_SCRM(n0=300000, n1=50000, nstep=10000)
